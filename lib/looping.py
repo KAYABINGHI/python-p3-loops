b9fccc2c2730e8b471a0726ceb39bb889e80a36a
KAYABINGHI <muchaipauls@gmail.com>
2025-08-07 21:18:11 +0300

    lupin

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    # Start at 10 and count down to 1
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    # Create a new list of squared integers using a for loop
+    squared = []
+    for num in int_list:
+        squared.append(num ** 2)
+    return squared
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    # Print numbers from 1 to 100 with FizzBuzz logic
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+happy_new_year()
+print(square_integers([1, 2, 3, 4, 5]))
+fizzbuzz()
